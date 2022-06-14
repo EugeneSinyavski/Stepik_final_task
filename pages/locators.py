99158242fb3abb40d3afc0a190866995263889ec
EugeneSianko <sinyavskis@mail.ru>
2022-06-14 12:06:49 +0300

    moved methods from main_page to base_page

--- pages/locators.py
+++ pages/locators.py
@@ -10,8 +10,14 @@ class LoginPageLocators:
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
 
-class ProductPageLokators:
+class ProductPageLocators:
     BASKET = (By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.btn-add-to-basket")
     ALERTINNER_ELEMENTS = (By.CSS_SELECTOR, "div.alertinner strong")
     NAME_BOOK = (By.CSS_SELECTOR, "div.col-sm-6.product_main h1")
     PRICE = (By.CSS_SELECTOR, "p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alert.alert-safe.alert-noicon.alert-success.fade.in")
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,15 +1,25 @@
 from .base_page import BasePage
-from .locators import ProductPageLokators
+from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
     def guest_can_add_product_to_basket(self):
-        basket = self.browser.find_element(*ProductPageLokators.BASKET)
+        basket = self.browser.find_element(*ProductPageLocators.BASKET)
         basket.click()
 
     def allert_guest_add_product_to_basket(self):
-        alertinner_elements = self.browser.find_elements(*ProductPageLokators.ALERTINNER_ELEMENTS)
-        name_book = self.browser.find_element(*ProductPageLokators.NAME_BOOK)
-        price = self.browser.find_element(*ProductPageLokators.PRICE)
-        assert name_book.text == alertinner_elements[0].text, "lohi pedal"
-        assert price.text == alertinner_elements[2].text, "gggfgf"
+        alertinner_elements = self.browser.find_elements(*ProductPageLocators.ALERTINNER_ELEMENTS)
+        name_book = self.browser.find_element(*ProductPageLocators.NAME_BOOK)
+        price = self.browser.find_element(*ProductPageLocators.PRICE)
+        assert name_book.text == alertinner_elements[0].text, \
+            "the name of the book in the cart does not match the product page"
+        assert price.text == alertinner_elements[2].text, \
+            "the price of the product does not match the price of the product in the cart"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_be_disappeared_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared, but should be"
