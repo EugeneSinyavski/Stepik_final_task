d573c2dbc42b0600617de73190fcd38c5a278cf7
EugeneSianko <sinyavskis@mail.ru>
2023-06-21 11:44:34 +0300

    Rewrote conftest.py: replaced yield with request

--- conftest.py
+++ conftest.py
@@ -16,15 +16,20 @@ def browser(request):
     user_language = request.config.getoption("language")
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=options)
+        chrome_options = Options()
+        chrome_options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=chrome_options)
 
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox()
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
+
+    def finalizer():
+        print("\nquit browser..")
+        browser.quit()
+
+    request.addfinalizer(finalizer)
+
+    return browser
